00f8b3291aa1b94a204d84b8c266d37817770f7c
Daniel <Dani45>
2019-02-07 11:56:21 +0100

    Creación de repositorio y esqueleto para la practica de DAD

---
+++ BiblioSpringUrjc/src/main/java/com/BiblioSpring/BiblioSpringUrjcApplication.java
@@ -0,0 +1,14 @@
+package com.BiblioSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiblioSpringUrjcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiblioSpringUrjcApplication.class, args);
+	}
+
+}
+
---
+++ BiblioSpringUrjc/src/test/java/com/BiblioSpring/BiblioSpringUrjcApplicationTests.java
@@ -0,0 +1,17 @@
+package com.BiblioSpring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BiblioSpringUrjcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
