e225e1efcb61376f2076840f426aa318c3a4f162
agustinlopezg <46849193+agustinlopezg@users.noreply.github.com>
2019-03-01 02:02:18 +0100

    Todo lo referido a la parte de Alternativas
    Controladores, Entidades, Repositorios y Templates de la parte de Alternativa en la app.
    Contiene todas las funcionalidades sobre ellas: añadir alternativas, ver las ya creadas, etc.

---
+++ Alternativas - C,E,R,T - FASE 2y3/Entidades/Alternativa.java
@@ -0,0 +1,76 @@
+package com.ejemplo.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Alternativa {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long idAlternativa;
+
+	@Column
+	private String alternativa;
+	
+	@OneToOne
+	private Fanzine fanzine;
+	@OneToOne
+	private Revista revista;
+	@OneToOne
+	private Pelicula pelicula;
+
+
+	public Alternativa() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Alternativa(String alternativa) {
+		super();
+		this.alternativa = alternativa;
+	}
+	public long getIdAlternativa() {
+		return idAlternativa;
+	}
+	public void setIdAlternativa(long idAlternativa) {
+		this.idAlternativa = idAlternativa;
+	}
+	public String getAlternativa() {
+		return alternativa;
+	}
+	public void setAlternativa(String alternativa) {
+		this.alternativa = alternativa;
+	}
+	public Fanzine getFanzine() {
+		return fanzine;
+	}
+	public void setFanzine(Fanzine fanzine) {
+		this.fanzine = fanzine;
+	}
+	public Revista getRevista() {
+		return revista;
+	}
+	public void setRevista(Revista revista) {
+		this.revista = revista;
+	}
+	public Pelicula getPelicula() {
+		return pelicula;
+	}
+	public void setPelicula(Pelicula pelicula) {
+		this.pelicula = pelicula;
+	}
+	@Override
+	public String toString() {
+		return "Alternativa [alternativa=" + alternativa + ", fanzine=" + fanzine + ", revista=" + revista
+				+ ", pelicula=" + pelicula + "]";
+	}
+	
+	
+
+}
---
+++ Alternativas - C,E,R,T - FASE 2y3/Repositorios/RevistasRepository.java
@@ -0,0 +1,8 @@
+package com.ejemplo.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ejemplo.entity.Revista;
+
+public interface RevistasRepository extends JpaRepository<Revista,Long>{
+
+}
