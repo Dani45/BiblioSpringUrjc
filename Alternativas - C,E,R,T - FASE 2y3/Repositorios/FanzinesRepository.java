d0c82ac49e6de17d95fbb604e3cd612dc8f1db55
Oscarli1797 <46781765+Oscarli1797@users.noreply.github.com>
2019-03-03 18:45:49 +0100

    versión antigua del entidad

--- Alternativas - C,E,R,T - FASE 2y3/Repositorios/FanzinesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ejemplo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ejemplo.entity.Fanzine;
-
-public interface FanzinesRepository extends JpaRepository<Fanzine, Long> {
-
-}
