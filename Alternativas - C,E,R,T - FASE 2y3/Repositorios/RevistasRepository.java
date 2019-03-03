277f1bb21c3c60d1877a0dd9b35b939905c11309
Oscarli1797 <46781765+Oscarli1797@users.noreply.github.com>
2019-03-03 18:46:15 +0100

    versión antigua del entidad

--- Alternativas - C,E,R,T - FASE 2y3/Repositorios/RevistasRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ejemplo.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ejemplo.entity.Revista;
-
-public interface RevistasRepository extends JpaRepository<Revista,Long>{
-
-}
