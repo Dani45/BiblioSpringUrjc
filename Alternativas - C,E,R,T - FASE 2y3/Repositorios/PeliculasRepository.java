0a280103db0ac0bb349bbda695cc3a647ba1cfb1
Oscarli1797 <46781765+Oscarli1797@users.noreply.github.com>
2019-03-03 18:46:02 +0100

    versión antigua del entidad

--- Alternativas - C,E,R,T - FASE 2y3/Repositorios/PeliculasRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ejemplo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ejemplo.entity.Pelicula;
-
-public interface PeliculasRepository extends JpaRepository<Pelicula, Long> {
-
-}
