59e34536255c6c84b135831f26713f4a8ded7b71
agustinlopezg <46849193+agustinlopezg@users.noreply.github.com>
2019-03-20 17:54:52 +0100

    Opciones de borrado añadidas

--- BiblioSpringUrjc/src/main/java/com/BiblioSpring/repository/FanzinesRepository.java
+++ BiblioSpringUrjc/src/main/java/com/BiblioSpring/repository/FanzinesRepository.java
@@ -3,10 +3,12 @@ package com.BiblioSpring.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
 import com.BiblioSpring.entity.Fanzine;
 
-public interface FanzinesRepository extends JpaRepository<Fanzine, Long> {
+public interface FanzinesRepository extends JpaRepository<Fanzine, Long> , CrudRepository<Fanzine,Long>{
 	Optional<Fanzine> findByNombre(String nombre);
 	Optional<Fanzine> findByLugarPublicacion(String lugarPublicacion);
+	Optional<Fanzine> deleteByNombre(String nombre);
 }
--- BiblioSpringUrjc/src/main/java/com/BiblioSpring/repository/RevistasRepository.java
+++ BiblioSpringUrjc/src/main/java/com/BiblioSpring/repository/RevistasRepository.java
@@ -2,11 +2,14 @@ package com.BiblioSpring.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
 import com.BiblioSpring.entity.Fanzine;
+import com.BiblioSpring.entity.Pelicula;
 import com.BiblioSpring.entity.Revista;
 
-public interface RevistasRepository extends JpaRepository<Revista,Long>{
+public interface RevistasRepository extends JpaRepository<Revista,Long>, CrudRepository<Revista,Long>{
 	Optional<Revista> findByNombre(String nombre);
 	Optional<Revista> findByAno(Integer ano);
+	Optional<Revista> deleteByNombre(String nombre);
 }
